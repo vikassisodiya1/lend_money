aea691f2f1ab7fbeb0568372b63436fc1847316c
vikassisodiya1 <sisodiyav3@gmail.com>
2024-07-28 15:26:46 +0530

    initial commit

---
+++ app/controllers/concerns/json_web_token_validation.rb
@@ -0,0 +1,29 @@
+  module JsonWebTokenValidation
+    ERROR_CLASSES = [
+      JWT::DecodeError,
+      JWT::ExpiredSignature,
+    ].freeze
+
+    private
+
+    def validate_json_web_token
+      token = request.headers[:token] || params[:token]
+      
+      begin
+        @token = JsonWebToken.decode(token)
+      rescue *ERROR_CLASSES => exception
+        handle_exception exception
+      end
+    end
+
+    def handle_exception(exception)
+      case exception
+      when JWT::ExpiredSignature
+        return render json: { errors: [token: 'Token has Expired'] },
+          status: :unauthorized
+      when JWT::DecodeError
+        return render json: { errors: [token: 'Invalid token'] },
+          status: :bad_request
+      end
+    end
+  end
